286893255753d07d811331e1b8e1c7244f5dc918
guantai <jpguantai@gmail.com>
2023-01-28 15:22:42 +0300

    final

--- script.js
+++ script.js
@@ -3,10 +3,41 @@ var sidemenu = document.getElementById("sidemenu");
 
 function openmenu(){
   sidemenu.style.right="0";
-  
+
+const icon = document.getElementById("openbtn")
+if(window.innerWidth <= 768){
+  icon.style.display="block";
+}else{
+  icon.style.display="none";
+
+ 
 }
+
+
+}
+
+
+
 function closemenu(){
-  sidemenu.style.right="-200px";
+  sidemenu.style.right="-300px";
+//  const icon = document.getElementById("closebtn")
+//  //if the window is less than or equal to 768px wide, display the icon
+//   if(window.innerWidth <= 768){
+//   icon.style.display="block";
+// }else{
+//   icon.style.display="none";
+
+  
   
 }
+
+
+window.onresize = closemenu
+window.onresize = openmenu
+window.onload = openmenu
+
+
+
+
+
 
